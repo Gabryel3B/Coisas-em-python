674c8ce7c9c22d266594d31095edf86c695cf8e3
Gabryel <141295068+Gabryel3B@users.noreply.github.com>
2024-04-28 01:56:03 -0300

    Add files via upload

---
+++ Coisas simples/helloworldelaborado.py
@@ -0,0 +1,14 @@
+alfabeto = "abcdefghijklmnopqrstuvwxyz !"
+
+letra1 = alfabeto[7]
+letra2 = alfabeto[4]
+letra3410 = alfabeto[11]
+letra58 = alfabeto[14]
+letra6 = alfabeto[26]
+letra7 = alfabeto[22]
+letra9 = alfabeto[17]
+letra10 = alfabeto[3]
+letra11 = alfabeto[27]
+
+print(letra1.capitalize()+letra2+letra3410*2+letra58+letra6+letra7.capitalize()+letra58+letra9+letra3410+letra10+letra11)
+
